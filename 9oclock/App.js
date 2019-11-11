e29bd68fe82b6090ae8b35cce3ff1b65ebfcae5c
wsh4872 <cowgirl409@naver.com>
2019-11-11 12:25:52 +0900

    mypage merge

--- 9oclock/App.js
+++ 9oclock/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import SignUpPage from './page/SignUp.js'
-
+import Mainpage from "./mypage/Mypagemain";
 export default class App extends React.Component {
   render() {
     return (
-      <SignUpPage />
+      <Mainpage />
+      // <SignUpPage />
+      
     );
 
   }
