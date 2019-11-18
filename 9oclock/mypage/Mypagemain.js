0d07e9547139e5758abf7ac0a50a8baed856e234
wsh4872 <cowgirl409@naver.com>
2019-11-19 01:47:33 +0900

    회원정보 gui 완성

--- 9oclock/mypage/Mypagemain.js
+++ 9oclock/mypage/Mypagemain.js
@@ -39,17 +39,91 @@ export default class Mainpage extends Component{
             />
 
             </View>
-            <Hr 
-              lineColor = {"#a9a9a9"}
-              thickness = {2}
-              hrPadding = {10}
-              
-            />
-           
-              
+            <View style={styles.otherpages}>
+            <Text>                                                                                                          </Text>
+              </View>
+        <View style={styles.otherpages}>
+        <TouchableOpacity onPress={this.appinfo}>
+          <Text style={styles.otherlink}>앱 정보                                                                          </Text>
+       </TouchableOpacity>
+       </View>
+
+       <View style={styles.otherpages}>
+       <TouchableOpacity onPress={this.pwchange}>
+          <Text style={styles.otherlink}>비밀번호 변경                                                                 </Text>
+       </TouchableOpacity>
+       </View>
+
+       <View style={styles.otherpages}>
+       <TouchableOpacity onPress={this.accountchange}>
+          <Text style={styles.otherlink}>계좌번호 변경                                                                 </Text>
+       </TouchableOpacity>
+       </View>
+
+       <View style={styles.otherpages}>
+       <TouchableOpacity onPress={this.logout}>
+          <Text style={styles.otherlink}>로그아웃                                                                      </Text>
+       </TouchableOpacity>
+       </View>
+
+       <View style={styles.otherpages}>
+       <TouchableOpacity onPress={this.accountdrop}>
+          <Text style={styles.otherlink}>회원 탈퇴                                                                      </Text>
+       </TouchableOpacity>
+       </View>
+      
+       <View style={styles.otherpages}>
+       <TouchableOpacity onPress={this.appnoti}>
+          <Text style={styles.otherlink}>앱 알림                                                                      </Text>
+       </TouchableOpacity>
+       </View>
         </View>
+       
         );
 
+    }
+    
+    appinfo = () =>{
+      this.setState(prevState =>{ 
+        return{
+          trial : false,
+        }
+      }); 
+    }
+    pwchange = () =>{
+      this.setState(prevState =>{ 
+        return{
+          trial : false,
+        }
+      }); 
+    }
+    accontchange = () =>{
+      this.setState(prevState =>{ 
+        return{
+          trial : false,
+        }
+      }); 
+    }
+    logout = () =>{
+      this.setState(prevState =>{ 
+        return{
+          trial : false,
+        }
+      }); 
+    }
+    accountdrop = () =>{
+      this.setState(prevState =>{ 
+        return{
+          trial : false,
+        }
+      }); 
+    }
+    appnoti = () =>{
+      this.setState(prevState =>{ 
+        return{
+          trial : false,
+        }
+      }); 
     }
     xbutton = () => {
       //페이지 이동 , 일단은 누르는 버튼으로 만들어 놨음.
@@ -75,19 +149,24 @@ const styles = StyleSheet.create({
         fontWeight : "900",     
         alignSelf : "center"
       },
-        
-      input:{
-        marginTop: 50,
+      otherpages : {
+        alignSelf : "flex-start",
         marginHorizontal : 10,
         padding: 5,
         borderBottomColor:"#bbb",
         borderBottomWidth: 2,
         alignSelf : "flex-start",
-        flexDirection:"row",
         justifyContent:"space-between"
-    
       },
-      firstline:{
+      otherlink : {
+        fontSize : 20,
+        marginLeft : 5,
+        marginTop : 10,
+        marginBottom : 10
+
+      },
+      input:{
+        marginTop: 50,
         marginHorizontal : 10,
         padding: 5,
         borderBottomColor:"#bbb",
